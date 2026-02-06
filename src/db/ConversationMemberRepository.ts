36438bbb263124d73fe766c944aadf9d1327c6dc
baris.alis <baris.alis@wire.com>
2026-02-06 17:42:55 +0100

    Change variable naming

--- src/db/ConversationMemberRepository.ts
+++ src/db/ConversationMemberRepository.ts
@@ -117,12 +117,12 @@ export class ConversationMemberRepository {
   }
 
   deleteMany(userIds: QualifiedId[], conversationId: string, conversationDomain: string): void {
-    const deleteMany = this.database.db.transaction((userIds) => {
+    const deleteUsersTransaction = this.database.db.transaction((userIds) => {
       for (const userId of userIds) {
         this.delete(userId.id, userId.domain, conversationId, conversationDomain)
       }
     })
-    deleteMany(userIds)
+    deleteUsersTransaction(userIds)
   }
 
   deleteAllMembersInConversation(conversationId: string, conversationDomain: string) {
