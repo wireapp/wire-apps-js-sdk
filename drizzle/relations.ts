8519185f4720d3c4c2679ea296bcb8e4f1f3f583
MarianKijewski <mariankijewski@outlook.com>
2026-03-13 16:42:09 +0100

    Introspect schema and relations using `drizzle-kit pull` command

---
+++ drizzle/relations.ts
@@ -0,0 +1,13 @@
+import { relations } from "drizzle-orm/relations";
+import { conversation, conversationMember } from "./schema";
+
+export const conversationMemberRelations = relations(conversationMember, ({one}) => ({
+	conversation: one(conversation, {
+		fields: [conversationMember.conversationId],
+		references: [conversation.id]
+	}),
+}));
+
+export const conversationRelations = relations(conversation, ({many}) => ({
+	conversationMembers: many(conversationMember),
+}));
---
+++ drizzle/schema.ts
@@ -0,0 +1,39 @@
+import { sqliteTable, AnySQLiteColumn, text, primaryKey, foreignKey } from "drizzle-orm/sqlite-core"
+import { sql } from "drizzle-orm"
+
+export const appProperties = sqliteTable("app_properties", {
+  key: text().primaryKey().notNull(),
+  value: text().notNull(),
+  creationDate: text().default("sql`(CURRENT_TIMESTAMP)`").notNull(),
+});
+
+export const conversation = sqliteTable("conversation", {
+    id: text().notNull(),
+    domain: text().notNull(),
+    name: text(),
+    teamId: text(),
+    mlsGroupId: text().notNull(),
+    creationDate: text().default("sql`(CURRENT_TIMESTAMP)`").notNull(),
+    type: text().notNull(),
+  },
+  (table) => [
+    primaryKey({ columns: [table.id, table.domain], name: "conversation_id_domain_pk"})
+  ]);
+
+export const conversationMember = sqliteTable("conversation_member", {
+    userId: text().notNull(),
+    userDomain: text().notNull(),
+    conversationId: text().notNull(),
+    conversationDomain: text().notNull(),
+    role: text().notNull(),
+    creationDate: text().default("sql`(CURRENT_TIMESTAMP)`").notNull(),
+  },
+  (table) => [
+    foreignKey(() => ({
+      columns: [table.conversationId, table.conversationDomain],
+      foreignColumns: [conversation.id, conversation.domain],
+      name: "conversation_member_conversationId_conversationDomain_conversation_id_domain_fk"
+    })),
+    primaryKey({ columns: [table.userId, table.userDomain, table.conversationId, table.conversationDomain], name: "conversation_member_userId_userDomain_conversationId_conversationDomain_pk"})
+  ]);
+
