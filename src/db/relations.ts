ab564cc4bffa6bc41f8cbb73511ff1c49b3384ea
MarianKijewski <mariankijewski@outlook.com>
2026-03-18 18:51:31 +0100

    Apply linter to generated files

--- src/db/relations.ts
+++ src/db/relations.ts
@@ -2,12 +2,12 @@ import { relations } from "drizzle-orm/relations";
 import { conversation, conversationMember } from "./schema";
 
 export const conversationMemberRelations = relations(conversationMember, ({one}) => ({
-	conversation: one(conversation, {
-		fields: [conversationMember.conversationId],
-		references: [conversation.id]
-	}),
+  conversation: one(conversation, {
+    fields: [conversationMember.conversationId],
+    references: [conversation.id]
+  }),
 }));
 
 export const conversationRelations = relations(conversation, ({many}) => ({
-	conversationMembers: many(conversationMember),
+  conversationMembers: many(conversationMember),
 }));
