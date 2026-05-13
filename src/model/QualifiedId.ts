a0a6fbbaac041e1407ca5196f101ef39ed16ac10
baris.alis <baris.alis@wire.com>
2026-05-13 09:37:57 +0200

    convert QualifiedId to class. Override toString and embed obfuscate in it.

--- src/model/QualifiedId.ts
+++ src/model/QualifiedId.ts
@@ -1,7 +1,7 @@
 /*
 * Wire
 * Copyright (C) 2025 Wire Swiss GmbH
-* 
+*
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
 * the Free Software Foundation, either version 3 of the License, or
@@ -14,7 +14,15 @@
 * along with this program. If not, see http://www.gnu.org/licenses/.
 */
 
-export interface QualifiedId {
-  id: string
-  domain: string
+import {obfuscateId} from "../utils/ObfuscateUtil.js";
+
+export class QualifiedId {
+  constructor(
+    readonly id: string,
+    readonly domain: string
+  ) {}
+
+  toString(): string {
+    return `${obfuscateId(this.id)}@${this.domain}`
+  }
 }
