98737f99311fb4f38e0213a93bda78f41b1ce66f
Alexandre Ferris <ferris.alexandre@gmail.com>
2026-06-05 09:18:20 +0200

    feat: Support ButtonAction message type #WPB-25696 (#196)

--- src/model/WireMessage.ts
+++ src/model/WireMessage.ts
@@ -100,6 +100,7 @@ export interface TextMessage extends WireMessageBase, Item, Ephemeral, Replyable
 export const TextMessage = {
   create(
     params: {
+      // TODO(alexandre): add messageId
       conversationId: QualifiedId
       text: string
       mentions?: Mention[]
@@ -109,13 +110,13 @@ export const TextMessage = {
   ): TextMessage {
     return {
       type: 'text',
-      id: crypto.randomUUID(),
+      id: crypto.randomUUID(), // TODO(alexandre): add messageId
       conversationId: params.conversationId,
       sender: new QualifiedId(crypto.randomUUID(), crypto.randomUUID()),
       text: params.text,
       mentions: params.mentions ?? [],
       linkPreviews: params.linkPreviews ?? [],
-      timestamp: new Date(),
+      timestamp: new Date(), // TODO(alexandre): only from replyable type
       expiresAfterMillis: params.expiresAfterMillis ?? null
     }
   }
@@ -147,7 +148,7 @@ export const AssetMessage = {
       id: crypto.randomUUID(),
       conversationId: params.conversationId,
       sender: new QualifiedId(crypto.randomUUID(), crypto.randomUUID()),
-      timestamp: new Date(),
+      timestamp: new Date(), // TODO(alexandre): only from replyable type
       sizeInBytes: params.sizeInBytes,
       name: params.name ?? null,
       mimeType: params.mimeType,
@@ -209,8 +210,35 @@ export const CompositeButton = {
   }
 }
 
+export interface CompositeButtonAction extends WireMessageBase {
+  type: "composite_button_action"
+  referenceMessageId: string
+  buttonId: string
+}
+
+export const CompositeButtonAction = {
+  create(
+    params: {
+      messageId: string
+      conversationId: QualifiedId
+      referenceMessageId: string
+      buttonId: string
+    }
+  ): CompositeButtonAction {
+    return {
+      type: 'composite_button_action',
+      id: params.messageId,
+      conversationId: params.conversationId,
+      buttonId: params.buttonId,
+      referenceMessageId: params.referenceMessageId,
+      sender: new QualifiedId(crypto.randomUUID(), crypto.randomUUID()), // TODO(alexandre): change to real sender
+      timestamp: new Date(), // TODO(alexandre): only from replyable type
+    }
+  }
+}
 
 export type WireMessage =
   | Unknown
   | TextMessage
-  | AssetMessage;
+  | AssetMessage
+  | CompositeButtonAction;
