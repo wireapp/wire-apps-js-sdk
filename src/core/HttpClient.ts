wireapp/wire-apps-js-sdk

commit c6c90e03d814b0bba3b56a11fd99dd014d8fd73a

feat: WPB-24732 Integrate app token authentication (#190)
* Scaffold unit tests

This outlines implementation requirements using descriptive test names.

* Add dependency for mocking http network requests

`msw` - Mock Service Worker
API recommended by Vitest: https://vitest.dev/guide/mocking/requests

* Test that access token received from BE is saved

The precondition of having cookie set is missing,
as cookie is now obtained by logging in,
which will be replaced by using provided App token.

* Replace login + password with api token

* Include API version in REST test handler

* fix: Wrong return value for mock

* Test cookie init

Initially cookie must be set from constructor parameters.
After it's refreshed it mustn't be overwritten by param.

* Add method for persisting cookie on DB

* Set backend token from constructor param

* Test if cookie from db is actually used in request

* Use cookie from DB in request

* Encrypt stored cookie with aes cbc

* Test that new cookie is set when received in response

* Rotate cookie from response

* Test client registration and access token relation

After client is registered and access endpoint is called,
the obtained access token should be full-fledged.

* Test expired cookie is erased after failing request

* Erase cookie from db after expiration

* Add todo for specific client exception handling

* Map standard error response to exception

This way it's possible to read error label
 and only erase cookie when it's expired

* Fix typos

* Log first app start with new API Token

* Get token expiry time from response

Previously hardcoded value was used.

* fix: Throw unhandled exception when cookie is expired

* Verify access token by default

For unauthorized endpoints this is skipped.

* Make key name private for app properties class

* fix: Initialization vector being encrypted

The iv was XORed by itself which caused it to be constant (all zeros) every time.

* Make app properties test independent of AESUtils

src/core/HttpClient.ts
@@ -14,16 +14,18 @@
 * along with this program. If not, see http://www.gnu.org/licenses/.
 */
 
-import type {AccessResponse} from "../api/response/AccessResponse.js"
-import {WIRE_API_HOST, WIRE_USER_EMAIL, WIRE_USER_PASSWORD} from "../utils/DependencyInjectionTokens.js"
+import {WIRE_API_HOST} from "../utils/DependencyInjectionTokens.js"
 import type {WireApiError} from "../model/exception/WireApiError.js"
 import {inject, singleton} from "tsyringe"
 import {LoggerFactory} from "../utils/logger/LoggerFactory.js";
+import {AppProperties} from "../service/AppProperties.js";
+import {WireApiException} from "../model/exception/WireApiException.js";
+import type {AccessResponse} from "../api/response/AccessResponse.js";
 
 @singleton()
 export class HttpClient {
   private logger = LoggerFactory.getLogger(this.constructor.name)
-  private tokenTimestamp: number | null = null
+  private tokenExpirationTimestamp: number | null = null
   private cachedAccessToken: string | null = null
   private cachedDeviceId: string | null = null
   private headers: Record<string, string> = {
@@ -32,12 +34,15 @@ export class HttpClient {
 
   constructor(
     @inject(WIRE_API_HOST) private wireApiHost: string,
-    @inject(WIRE_USER_EMAIL) private wireUserEmail: string,
-    @inject(WIRE_USER_PASSWORD) private wireUserPassword: string
+    private appProperties: AppProperties
   ) {}
 
   private setAuthorizationToken(token: string) {
     this.headers["Authorization"] = `Bearer ${token}`
+  }
+
+  clearAuthorizationToken() {
+    this.cachedAccessToken = null
   }
 
   getApiHostVersion(): string {
@@ -70,56 +75,53 @@ export class HttpClient {
     const currentTime = Date.now()
 
     // Check if token is valid (not null and not expired)
-    if (this.cachedAccessToken != null && this.tokenTimestamp != null) {
-      const timeSinceTokenIssued = currentTime - this.tokenTimestamp
-      if (timeSinceTokenIssued < this.TOKEN_EXPIRATION_MS) {
-        this.setAuthorizationToken(this.cachedAccessToken)
-        return
-      }
+    if (
+      this.cachedAccessToken != null &&
+      this.tokenExpirationTimestamp != null &&
+      currentTime < this.tokenExpirationTimestamp
+    ) { return }
+    this.logger.info("Access token expired, getting a new one.")
 
-      this.logger.info("Access token expired, getting a new one.")
-    }
-
-    // TODO: Move to Token retrieval once backend tickets are done.
-    const loginResponse = (await this.request<Record<string, unknown>>("login", {
-      method: "POST",
-      body: JSON.stringify({
-        email: this.wireUserEmail,
-        password: this.wireUserPassword
-      }),
-      headers: {
-        "Content-Type": this.HEADER_DEFAULT_CONTENT_TYPE,
-      }
-    }))
-
-    if (this.cachedDeviceId != null) {
-      const setCookieHeaders: string[] = loginResponse.response.headers.getSetCookie();
-      const zuidCookie = setCookieHeaders
-        ?.find((cookie: string) => cookie.startsWith('zuid='))
-        ?.split(';')[0];
-
-      const path = this.cachedDeviceId
-        ? `access?client_id=${this.cachedDeviceId}`
-        : `access`
-      const jsonAccessResponse = (await this.request<Record<string, unknown>>(path, {
+    const path = this.cachedDeviceId
+      ? `access?client_id=${this.cachedDeviceId}`
+      : `access`
+    try {
+      const accessResponse = (await this.request<AccessResponse>(path, {
         method: "POST",
         headers: {
-          "Cookie": `${zuidCookie}`
+          "Cookie": `zuid=${this.appProperties.getBackendCookie()}`
         }
-      })).data
+      }))
 
-      const accessResponse: AccessResponse = {
-        accessToken: jsonAccessResponse["access_token"] as string,
-        expiresIn: jsonAccessResponse["expires_in"] as number
+      const setCookieHeaders: string[] = accessResponse.response.headers.getSetCookie();
+      const zuidCookie = setCookieHeaders
+        ?.find((cookie: string) => cookie.startsWith('zuid='))
+        ?.split(';')[0]
+        ?.slice(5); // remove "zuid="
+      if (zuidCookie)
+        this.appProperties.saveBackendCookie(zuidCookie)
+
+      const accessToken = accessResponse.data.access_token
+
+      this.cachedAccessToken = accessToken
+      this.tokenExpirationTimestamp = currentTime + (accessResponse.data.expires_in * 1000) // seconds to milliseconds
+
+      this.setAuthorizationToken(accessToken)
+    } catch (exception) {
+      this.logger.error('Unable to retrieve access token, Error:', exception)
+      if (exception instanceof WireApiException && exception.isCredentialsInvalid()) {
+        this.appProperties.deleteBackendCookie()
+
+        // TODO: Map to WireException
+        throw new Error("Current cookie/api-token is expired. Get a new apiToken and restart the App")
       }
-
-      this.cachedAccessToken = accessResponse.accessToken
-      this.tokenTimestamp = currentTime
-
-      this.setAuthorizationToken(accessResponse.accessToken)
-    } else {
-      this.setAuthorizationToken(loginResponse.data["access_token"] as string)
     }
+  }
+
+  private isAuthenticatedPath(path: string): boolean {
+    return !this.unauthenticatedPaths.some(prefix =>
+      path.startsWith(prefix)
+    );
   }
 
   async request<T>(
@@ -127,6 +129,8 @@ export class HttpClient {
     options: RequestInit = {},
     includeApiVersion: boolean = true
   ): Promise<{ data: T; response: Response }> {
+    if (this.isAuthenticatedPath(path))
+      await this.verifyAuthorizationToken()
     const optionsAndHeaders = {
       ...options,
       headers: {
@@ -142,23 +146,24 @@ export class HttpClient {
     const response = await fetch(url, optionsAndHeaders)
 
     if (!response.ok) {
-      let errorDetails = ''
+      let standardError: WireApiError | undefined
 
-      try {
-        const contentType = response.headers.get("content-type")
-        if (contentType?.includes("application/json")) {
-          const errorBody = await response.json() as Partial<WireApiError>
-          if (errorBody.label && errorBody.message) {
-            this.logger.error(`API Error - Label: ${errorBody.label}, Message: ${errorBody.message}`)
-            errorDetails = ` [${errorBody.label}]: ${errorBody.message}`
-          }
+      const contentType = response.headers.get("content-type")
+      if (contentType?.includes("application/json")) {
+        try {
+          standardError = await response.json() as WireApiError
+        } catch (exception) {
+          this.logger.error(`Could not parse error response: ${exception}`)
         }
-      } catch (exception) {
-        this.logger.error(`Could not parse error response: ${exception}`)
+      }
+
+      if (standardError?.label && standardError?.message) {
+        this.logger.error(`WireApiException - Label: ${standardError.label}, Message: ${standardError.message}`)
+        throw new WireApiException(standardError)
       }
 
       // TODO: Map to WireException
-      throw new Error(`HTTP ${response.status} for ${path}${errorDetails || ': ' + response.statusText}`)
+      throw new Error(`HTTP ${response.status} for ${path}: ${response.statusText}`)
     }
 
     const contentType = response.headers.get("content-type")
@@ -186,8 +191,6 @@ export class HttpClient {
       additionalHeaders?: Record<string, string>
     }
   ): Promise<T> {
-    await this.verifyAuthorizationToken()
-
     const {
       headerContentType = this.HEADER_DEFAULT_CONTENT_TYPE,
       headerAccept = this.HEADER_DEFAULT_ACCEPT,
@@ -245,7 +248,6 @@ export class HttpClient {
     headerContentType: string = this.HEADER_DEFAULT_CONTENT_TYPE,
     headerAccept: string = this.HEADER_DEFAULT_ACCEPT
   ): Promise<T> {
-    await this.verifyAuthorizationToken()
     return (await this.request<T>(path, {
       method: "PUT",
       body: JSON.stringify(body),
@@ -264,8 +266,6 @@ export class HttpClient {
       headerAccept?: string;
     }
   ): Promise<T> {
-    await this.verifyAuthorizationToken()
-
     const {
       headerContentType = this.HEADER_DEFAULT_CONTENT_TYPE,
       headerAccept = this.HEADER_DEFAULT_ACCEPT
@@ -287,7 +287,7 @@ export class HttpClient {
   }
 
   private API_HOST_VERSION: string = "v15"
-  private TOKEN_EXPIRATION_MS = 14 * 60 * 1000 // 14 minutes in milliseconds
   private HEADER_DEFAULT_CONTENT_TYPE = "application/json"
   private HEADER_DEFAULT_ACCEPT = "application/json"
+  private readonly unauthenticatedPaths = ['access', 'api-version']
 }

.env.example
@@ -1,6 +1,5 @@
-WIRE_SDK_USER_EMAIL=youremail@yourdomain.com
-WIRE_SDK_USER_PASSWORD=YourP@ssw0rd
 WIRE_SDK_USER_ID=your-user-ID-UUID-format
+WIRE_SDK_API_TOKEN=your-api-token
 WIRE_SDK_USER_DOMAIN=yourdomain
 WIRE_SDK_API_HOST=https://your-api.host
 WIRE_SDK_CRYPTO_PASSWORD=YourCryptographyStoragePassword

sample/src/app.ts
@@ -38,23 +38,18 @@ import path from 'node:path'
 
 dotenv.config({ path: '../.env' })
 
-const userEmail = process.env['WIRE_SDK_USER_EMAIL'];
-const userPassword = process.env['WIRE_SDK_USER_PASSWORD'];
 const userId = process.env['WIRE_SDK_USER_ID'];
+const apiToken = process.env['WIRE_SDK_API_TOKEN'];
 const userDomain = process.env['WIRE_SDK_USER_DOMAIN'];
 const apiHost = process.env['WIRE_SDK_API_HOST'];
 const cryptographyStoragePassword = process.env['WIRE_SDK_CRYPTO_PASSWORD'];
 
-if (!userEmail) {
-  throw new Error('WIRE_SDK_USER_EMAIL must be set in .env file');
-}
-
-if (!userPassword) {
-  throw new Error('WIRE_SDK_USER_PASSWORD must be set in .env file');
-}
-
 if (!userId) {
   throw new Error('WIRE_SDK_USER_ID must be set in .env file');
+}
+
+if (!apiToken) {
+  throw new Error('WIRE_SDK_API_TOKEN must be set in .env file');
 }
 
 if (!userDomain) {
@@ -446,9 +441,8 @@ class SampleEventsHandler extends WireEventsHandler {
 const sampleEventsHandler = new SampleEventsHandler()
 sampleEventsHandler.appLogger = new PinoLogger()
 const sdk = await WireAppSdk.create(
-  userEmail,
-  userPassword,
   userId,
+  apiToken,
   userDomain,
   apiHost,
   cryptographyStoragePassword,

src/WireAppSdk.ts
@@ -24,9 +24,8 @@ import {
   WIRE_DATABASE_PATH,
   WIRE_EVENTS_HANDLER,
   WIRE_USER_DOMAIN,
-  WIRE_USER_EMAIL,
   WIRE_USER_ID,
-  WIRE_USER_PASSWORD
+  WIRE_SDK_API_TOKEN
 } from "./utils/DependencyInjectionTokens.js";
 import {WebSocketClient} from "./core/WebSocketClient.js";
 import {WireEventsHandler} from "./core/WireEventsHandler.js";
@@ -36,11 +35,11 @@ import type {Logger} from "./utils/logger/Logger.js";
 import {LoggerFactory} from "./utils/logger/LoggerFactory.js";
 import {ConsoleLogger} from "./utils/logger/ConsoleLogger.js";
 import {ConversationService} from "./api/ConversationService.js";
+import {AppProperties} from "./service/AppProperties.js";
 
 export class WireAppSdk {
-  private userEmail: string
-  private userPassword: string
   private userId: string
+  private apiToken: string
   private userDomain: string
   private apiHost: string
   private cryptographyStoragePassword: string
@@ -50,23 +49,22 @@ export class WireAppSdk {
   private isWebSocketRunning: boolean = false
   private webSocketClient!: WebSocketClient
   private conversationService!: ConversationService
+  private appProperties!: AppProperties
 
   private wireEventsHandler: WireEventsHandler
   private logger: Logger
 
   private constructor(
-    userEmail: string,
-    userPassword: string,
     userId: string,
+    apiToken: string,
     userDomain: string,
     apiHost: string,
     cryptographyStoragePassword: string,
     wireEventsHandler: WireEventsHandler,
     logger?: Logger
   ) {
-    this.userEmail = userEmail
-    this.userPassword = userPassword
     this.userId = userId
+    this.apiToken = apiToken
     this.userDomain = userDomain
     this.apiHost = apiHost
     this.cryptographyStoragePassword = cryptographyStoragePassword
@@ -76,9 +74,8 @@ export class WireAppSdk {
   }
 
   static async create(
-    userEmail: string,
-    userPassword: string,
     userId: string,
+    apiToken: string,
     userDomain: string,
     apiHost: string,
     cryptographyStoragePassword: string,
@@ -86,9 +83,8 @@ export class WireAppSdk {
     logger?: Logger,
   ): Promise<WireAppSdk> {
     const wireAppSdk = new WireAppSdk(
-      userEmail,
-      userPassword,
       userId,
+      apiToken,
       userDomain,
       apiHost,
       cryptographyStoragePassword,
@@ -98,19 +94,28 @@ export class WireAppSdk {
 
     wireAppSdk.registerExitHandlers()
     await wireAppSdk.init()
-
     return wireAppSdk
   }
 
   private async init() {
     this.configureDependencies()
+
+    // TODO: Remove when core-crypto client is persisted in storage
+    // Workaround: Trigger api token to be loaded from the .env
+    // Required as the core-crypto client is initialized for each program execution.
+    // The api token contains client id (in `i=...` param) from previous program execution.
+    this.appProperties.deleteBackendCookie()
+
+    // Save cookie from constructor parameter only at first application start.
+    // Once BE provides new token, the one stored in `apiToken` will be obsolete.
+    this.appProperties.saveBackendCookieIfMissing(this.apiToken)
+
     await this.initCryptoClient()
   }
 
   private configureDependencies() {
     container.registerInstance(WIRE_API_HOST, this.apiHost)
-    container.registerInstance(WIRE_USER_EMAIL, this.userEmail)
-    container.registerInstance(WIRE_USER_PASSWORD, this.userPassword)
+    container.registerInstance(WIRE_SDK_API_TOKEN, this.apiToken)
     container.registerInstance(WIRE_USER_ID, this.userId)
     container.registerInstance(WIRE_USER_DOMAIN, this.userDomain)
     container.registerInstance(WIRE_CRYPTO_STORAGE_PASSWORD, this.cryptographyStoragePassword)
@@ -120,6 +125,7 @@ export class WireAppSdk {
 
     this.webSocketClient = container.resolve(WebSocketClient)
     this.conversationService = container.resolve(ConversationService)
+    this.appProperties = container.resolve(AppProperties)
   }
 
   private async initCryptoClient() {

src/api/ClientsApiClient.ts
@@ -19,16 +19,13 @@ import type {RegisterClientResponse} from "./response/RegisterClientResponse.js"
 import {RegisterClientRequest} from "./request/RegisterClientRequest.js";
 import type {MlsPublicKeys} from "../model/MlsPublicKeys.js";
 import type {ClientUpdateRequest} from "./request/ClientUpdateRequest.js";
-import {WIRE_USER_PASSWORD} from "../utils/DependencyInjectionTokens.js";
 import {mapToPreKeyRequest} from "../mappers/PreKeyMapper.js";
 import type {PreKeyCrypto} from "../model/PreKeyCrypto.js";
-import { inject, singleton } from "tsyringe";
+import {singleton} from "tsyringe";
 
 @singleton()
 export class ClientsApiClient {
-  constructor(
-    private httpClient: HttpClient,
-    @inject(WIRE_USER_PASSWORD) private wireUserPassword: string) {
+  constructor(private httpClient: HttpClient) {
   }
 
   private readonly basePath = "clients";
@@ -39,7 +36,6 @@ export class ClientsApiClient {
   ): Promise<string> {
 
     const requestPayload = new RegisterClientRequest(
-      this.wireUserPassword,
       mapToPreKeyRequest(proteusLastPreKey),
       proteusPreKeys.map((preKey) =>
         mapToPreKeyRequest(preKey)
@@ -50,6 +46,10 @@ export class ClientsApiClient {
       this.basePath,
       requestPayload
     )
+
+    // Register client is performed with an access_token having limited scope.
+    // Clear the token to force a refresh with the full-scope token for next requests.
+    this.httpClient.clearAuthorizationToken()
 
     this.httpClient.setDeviceId(response.id)
     return response.id

src/api/request/RegisterClientRequest.ts
@@ -1,7 +1,7 @@
 /*
 * Wire
 * Copyright (C) 2025 Wire Swiss GmbH
-* 
+*
 * This program is free software: you can redistribute it and/or modify
 * it under the terms of the GNU General Public License as published by
 * the Free Software Foundation, either version 3 of the License, or
@@ -18,7 +18,6 @@ import { CapabilitiesRequest } from "./CapabilitiesRequest.js"
 import type { PreKeyRequest } from "./PreKeyRequest.js"
 
 export class RegisterClientRequest {
-  password: string
   lastkey: PreKeyRequest
   prekeys: PreKeyRequest[]
   capabilities: CapabilitiesRequest[]
@@ -26,14 +25,12 @@ export class RegisterClientRequest {
   model: string
 
   constructor(
-    password: string,
     lastKey: PreKeyRequest,
     preKeys: PreKeyRequest[],
     capabilities: CapabilitiesRequest[] = this.DEFAULT_CAPABILITIES,
     type: string = this.DEFAULT_CLIENT_TYPE,
     model: string = this.DEFAULT_CLIENT_MODEL
   ) {
-    this.password = password
     this.lastkey = lastKey
     this.prekeys = preKeys
     this.capabilities = capabilities

src/db/AppPropertiesRepository.ts
@@ -24,6 +24,7 @@ import {eq, sql} from "drizzle-orm";
 export class AppPropertiesRepository {
   private selectByKeyStmt
   private insertStmt
+  private deleteStmt
 
   constructor(private readonly databaseService: DatabaseService) {
     this.selectByKeyStmt = this.databaseService.db.select().from(appProperties).where(
@@ -38,6 +39,10 @@ export class AppPropertiesRepository {
       target: appProperties.key,
       set: { value: sql.raw(`excluded.${appProperties.value.name}`)}
     }).prepare();
+
+    this.deleteStmt = this.databaseService.db.delete(appProperties).where(
+      eq(appProperties.key, sql.placeholder('key'))
+    ).prepare();
   }
 
   getByKey(key: string): AppEntity | undefined {
@@ -49,5 +54,9 @@ export class AppPropertiesRepository {
    */
   save(key: string, value: string): void {
     this.insertStmt.run({ key, value })
+  }
+
+  delete(key: string) {
+    this.deleteStmt.run({ key })
   }
 }

src/model/exception/WireApiException.ts
@@ -14,9 +14,20 @@
  * along with this program. If not, see http://www.gnu.org/licenses/.
  */
 
-export interface AccessResponse {
-  access_token: string
-  expires_in: number
-  token_type: string
-  user: string
+import type {WireApiError} from "./WireApiError.js";
+
+export class WireApiException extends Error {
+  readonly code: number;
+  readonly label: string;
+
+  constructor({ code, label, message }: WireApiError) {
+    super(message);
+    this.name = "WireApiException";
+    this.code = code;
+    this.label = label;
+  }
+
+  isCredentialsInvalid(): boolean {
+    return this.label === "invalid-credentials";
+  }
 }

commit b2e8e9fc8272f1c9a5dfa38a54fb85dc25380be1

fix: Remove repetitive proactive token verification
This in fact is a mistake that slided through when
app token authentication was first introduced in https://github.com/wireapp/wire-apps-js-sdk/pull/190

The fix should be applied in a separate PR, but that
would be redundant since proactive verification will
get removed altogether in this branch.

src/core/HttpClient.ts
@@ -218,8 +218,6 @@ export class HttpClient {
       includeApiVersion?: boolean;
     }
   ): Promise<T> {
-    await this.verifyAuthorizationToken()
-
     const {
       headerContentType = this.HEADER_DEFAULT_CONTENT_TYPE,
       headerAccept = this.HEADER_DEFAULT_ACCEPT,