6f211fa00d552622db2698bf4afba4f514ceab69
alexandreferris <ferris.alexandre@gmail.com>
2026-07-27 17:16:02 +0200

    chore: Prepare SDK for publishing #WPB-25720
    * Added module: build/index.js as a legacy ESM fallback
    * Added exports map for build/index.js + build/index.d.ts and package.json
    * Updated sample project to match new namings

--- sample/src/PinoLogger.ts
+++ sample/src/PinoLogger.ts
@@ -1,5 +1,5 @@
 import pino from 'pino';
-import { type Logger } from 'wire-apps-js-sdk'
+import { type Logger } from '@wireapp/wire-apps-js-sdk'
 
 export class PinoLogger implements Logger {
   private pinoLogger: pino.Logger;
